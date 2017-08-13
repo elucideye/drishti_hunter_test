a0ab327d3ef4e7eeae37848c49d9ca1ce1bd236c
David Hirvonen <dhirvonen@elucideye.com>
2017-08-13 19:17:39 -0400

    initial project setup

---
+++ drishti-eye-test.cpp
@@ -0,0 +1,31 @@
+#include <drishti/EyeSegmenter.hpp>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <cxxopts.hpp>
+
+int main(int argc, char **argv)
+{
+    const auto argumentCount = argc;
+    
+    std::string input, output;
+    
+    cxxopts::Options options("drishti-eye-test", "Command line interface for eye model fitting");
+    
+    // clang-format off
+    options.add_options()
+        // input/output:
+        ("i,input", "Input image", cxxopts::value<std::string>(input))
+        ("o,output", "Output image", cxxopts::value<std::string>(output))
+        ("model", "model", cxxopts::value<std::string>(model));
+    // clang-format on
+
+    options.parse(argc, argv);
+    if ((argumentCount <= 1) || options.count("help"))
+    {
+        std::cout << options.help({ "" }) << std::endl;
+        return 0;
+    }
+}
+
