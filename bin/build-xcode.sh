227e4af4d22bc907e079c39b35bfb15ac6f8599e
David Hirvonen <dhirvonen@elucideye.com>
2017-08-28 15:44:13 -0400

    add build script

---
+++ bin/build-xcode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TOOLCHAIN=xcode
+CONFIG=Release
+
+COMMAND=(
+    "--verbose "
+    "--fwd "
+    "HUNTER_CONFIGURATION_TYPES=${CONFIG} "    
+    "DRISHTI_BUILD_BENCHMARKS=OFF "
+    "DRISHTI_BUILD_TESTS=OFF "
+    "DRISHTI_BUILD_INTEGRATION_TESTS=OFF "
+    "DRISHTI_BUILD_SHARED_SDK=OFF "    
+    "--config ${CONFIG} "
+    "--jobs 8 "
+    "--install "
+)
+
+polly.py --toolchain ${TOOLCHAIN} ${COMMAND[@]} --verbose  --reconfig
