2677dd9ed91ae492126cb55529712b2b158311ed
David Hirvonen <dhirvonen@elucideye.com>
2017-08-28 12:47:28 -0400

    formatting bump hunter

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,3 +1,4 @@
+### Configure XGBOOST w/ cereal ###
 set(XGBOOST_CMAKE_ARGS
   XGBOOST_USE_HALF=ON
   XGBOOST_USE_CEREAL=ON
@@ -7,7 +8,7 @@ if(DRISHTI_TEST_BUILD_MIN_SIZE)
 else()
   list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=OFF)
 endif()
-
-
 hunter_config(xgboost VERSION ${HUNTER_xgboost_VERSION} CMAKE_ARGS ${XGBOOST_CMAKE_ARGS})
+
+### Configure drishti as submodule ###
 hunter_config(drishti GIT_SUBMODULE "src/3rdparty/drishti")
