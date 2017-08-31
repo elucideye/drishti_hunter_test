f12d87afac7578a5a281d2ad5b4291a5f798f602
David Hirvonen <dhirvonen@elucideye.com>
2017-08-31 10:57:21 -0400

    take union of local config and drishti-upload

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,14 +1,23 @@
-### Configure XGBOOST w/ cereal ###
-set(XGBOOST_CMAKE_ARGS
-  XGBOOST_USE_HALF=ON
-  XGBOOST_USE_CEREAL=ON
-  )
-if(DRISHTI_TEST_BUILD_MIN_SIZE)
-  list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=ON)
+if (EXISTS ${WORKING_CONFIG})
+
+  message("Has working config")
+  include(${WORKING_CONFIG}) # i.e., ${CMAKE_CURRENT_LIST_DIR}/drishti-upload/config.cmake
+  
 else()
-  list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=OFF)
+
+  ### Configure XGBOOST w/ cereal ###
+  set(XGBOOST_CMAKE_ARGS
+    XGBOOST_USE_HALF=ON
+    XGBOOST_USE_CEREAL=ON
+    )
+  if(DRISHTI_TEST_BUILD_MIN_SIZE)
+    list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=ON)
+  else()
+    list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=OFF)
+  endif()
+  hunter_config(xgboost VERSION ${HUNTER_xgboost_VERSION} CMAKE_ARGS ${XGBOOST_CMAKE_ARGS})
+
 endif()
-hunter_config(xgboost VERSION ${HUNTER_xgboost_VERSION} CMAKE_ARGS ${XGBOOST_CMAKE_ARGS})
 
 ### Configure drishti as submodule ###
 set(DRISHTI_CMAKE_ARGS
