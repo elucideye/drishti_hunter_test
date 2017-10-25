61f29105b8f5fb481680acfef101cacbdbaa8a36
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-10-25 15:35:32 +0300

    Remove unused

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,27 +1,14 @@
-# If ${WORKING_CONFIG} is set to a valid path, then we will use that as an initial
-# basis for a working set of hunter package configurations (i.e., hunter_config(...))
-# by a cmake include() and we can augment those definitions below, initially we do this
-# just for the drishti submodule.
-
-if (EXISTS ${WORKING_CONFIG})
-
-  include(${WORKING_CONFIG}) # i.e., ${CMAKE_CURRENT_LIST_DIR}/drishti-upload/config.cmake
-  
+### Configure XGBOOST w/ cereal ###
+set(XGBOOST_CMAKE_ARGS
+  XGBOOST_USE_HALF=ON
+  XGBOOST_USE_CEREAL=ON
+  )
+if(DRISHTI_TEST_BUILD_MIN_SIZE)
+  list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=ON)
 else()
-
-  ### Configure XGBOOST w/ cereal ###
-  set(XGBOOST_CMAKE_ARGS
-    XGBOOST_USE_HALF=ON
-    XGBOOST_USE_CEREAL=ON
-    )
-  if(DRISHTI_TEST_BUILD_MIN_SIZE)
-    list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=ON)
-  else()
-    list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=OFF)
-  endif()
-  hunter_config(xgboost VERSION ${HUNTER_xgboost_VERSION} CMAKE_ARGS ${XGBOOST_CMAKE_ARGS})
-
+  list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=OFF)
 endif()
+hunter_config(xgboost VERSION ${HUNTER_xgboost_VERSION} CMAKE_ARGS ${XGBOOST_CMAKE_ARGS})
 
 ### Configure drishti as submodule ###
 
