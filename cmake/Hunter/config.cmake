aedf8f75ef357ad29c431b219885769338cf3fe1
David Hirvonen <dhirvonen@elucideye.com>
2018-01-30 23:14:30 -0500

    update hunter, huntergate  (#10)
    * update hunter, huntergate and lock xgboost @  0.40-p10, use ACF_BUILD_OGLES_GPGPU=ON
    
    * update hunter
    
    * use older nlohmann_json for all ANDROID
    
    … targeting backwards compatibility for ndk10e + gcc toolchain incomplete -std=c++11 , to be removed with ndk17+clang+libc++ upgrade
    
    * travis xcode8.3 image for osx-10-12 toolchain
    
    * travis xcode8.3 image for osx-10-12 toolchain
    * bump hunter version

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -8,7 +8,13 @@ if(DRISHTI_TEST_BUILD_MIN_SIZE)
 else()
   list(APPEND XGBOOST_CMAKE_ARGS XGBOOST_DO_LEAN=OFF)
 endif()
-hunter_config(xgboost VERSION ${HUNTER_xgboost_VERSION} CMAKE_ARGS ${XGBOOST_CMAKE_ARGS})
+hunter_config(xgboost VERSION 0.40-p10 CMAKE_ARGS ${XGBOOST_CMAKE_ARGS})
+
+hunter_config(
+    acf
+    VERSION ${HUNTER_acf_VERSION}
+    CMAKE_ARGS ACF_BUILD_OGLES_GPGPU=ON
+)
 
 ### Configure drishti as submodule ###
 
@@ -16,8 +22,11 @@ if(DRISHTI_AS_SUBMODULE)
   hunter_config(drishti GIT_SUBMODULE "src/3rdparty/drishti")
 endif()
 
-# Workaround from:
-# * https://github.com/ruslo/hunter/commit/08d25c51e8fa3f3fcfa73f655fe3a7d85d1b4109
+# Workarounds for partial c++11 in ndk10e + gcc toolchains:
+# TODO: This could be managed by try_compile tests, but won't be needed after ndk17
 if(ANDROID)
+  # * https://github.com/ruslo/hunter/commit/08d25c51e8fa3f3fcfa73f655fe3a7d85d1b4109
   hunter_config(dlib VERSION 19.2-p1)
+  # * error: 'struct lconv' has no member named 'decimal_point'
+  hunter_config(nlohmann_json VERSION 2.1.1-p1)
 endif()
